f51c17e95cdbc508a3358cf4f13550086a55a7bd
jlamaral <159718926+JLAmaral@users.noreply.github.com>
2024-11-03 10:11:03 -0500

    Created hover effect Made a button the shake Etch-a-Sketch Made button to shake take user input for # of boxes

--- script.js
+++ script.js
@@ -2,21 +2,48 @@ const resetButton = document.querySelector('button')
 const container = document.querySelector('.container')
 
 const createGrid = (createGrid) => {
+  const wrapper = document.createElement('div')
+  wrapper.classList.add('wrapper')
+
   for (let i = 0; i < createGrid; i++) {
     const row = document.createElement('div')
     row.classList.add('grid-row')
 
     for (let j = 0; j < createGrid; j++) {
+    const widthAndHeight = 960 / createGrid
     const gridBox = document.createElement('div')
     gridBox.classList.add('grid-box')
+    gridBox.style.width = '${widthAndHeight}px'
+    gridBox.style.height = '${widthAndHeight}px'
+    // adding mouseenter listener to change background color
     gridBox.addEventListener('mouseenter', () => {
       gridBox.style.backgroundColor = 'black'
     })
-
     row.appendChild(gridBox)
-    }
-    container.appendChild(row)
+  }
+
+    wrapper.appendChild(row)
   } 
+  container.appendChild(wrapper)
 }
 
-createGrid(sizeOfGrid)
+resetButton.addEventListener('click', () => {
+  let userSize = Number(prompt('What dimensions do you want for the new grid'))
+
+  while (userSize > 100) {
+    userSize = Number(prompt('Pick a smaller number 100 or less'))
+  }
+
+  const wrapper = document.querySelector('.wrapper')
+
+  if (!wrapper) {
+    createGrid(userSize)
+  } else {
+    wrapper.remove()
+    createGrid(userSize)
+  }
+
+
+})
+
+createGrid(createGrid)
